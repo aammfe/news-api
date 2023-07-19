74569c9e00ae2ff97b8966b6ba82ef4fb2575f77
Abdul Hanan <abdul.hanan@platonic.systems>
2023-07-19 08:27:56 +0000

    add cache

--- README.md
+++ README.md
@@ -1,41 +1,64 @@
+
 # news-api
 
   
+
 News apis uses <https://newsapi.org/> as News Source. it has following endpoints,
 
+  
 
 ## Top-headlines
 
- Returns Top Headlinesit, it has flowing queryParams
+  
+
+Returns Top Headlinesit, it has flowing queryParams
+
+  
+
 
 | name | values | default | required |
 |---|---|---|---|
 | pageNumber | >0 | 1 | no |
 | pageSize | >0 | 10 | no |
+
+  
 
 example :`http://localhost:8080/top-headlines?&pageNumber=1&pageSize=1`
 
+  
 
 ## Query
 
- Returns queried news feed, it has flowing query params
+  
 
-| name | values | default | required |
+Returns queried news feed, it has flowing query params
+
+ | name | values | default | required |
 |---|---|---|---|
-| queryBy | title \| description \| content | - | yes |
-| query | keywords | - | yes |
+| queryBy | title \| description \| content |  | yes |
+| query | text/keywords |  | yes |
 | pageNumber | >0 | 1 | no |
 | pageSize | >0 | 10 | no |
-  
 
 example :`http://localhost:8080/query?queryBy=title&query=cardano&pageNumber=1&pageSize=1`
 
   
+
+## Cache
+
+  
+
+its using in Momery Cache, by default cache time is 10 minutes
+
   
 
 ## Getting Started
 
+  
+
 to Run `nix run . -- --newsApiKey api_key`
+
+  
 
 ## Tips
 
@@ -43,12 +66,18 @@ to Run `nix run . -- --newsApiKey api_key`
 
 - Run `nix flake update` to update all flake inputs.
 
+  
+
 - Run `nix run github:srid/nixci` to build _all_ outputs.
+
+  
 
 - Run `, fmt` in nix shell to autoformat the project. This uses [treefmt](https://github.com/numtide/treefmt).
 
+  
+
 - Run `, docs` to start Hoogle with packages in your cabal file.
 
-- Run the application without installing: `nix run github:aammfe/news-api` (or `nix run .` from checkout)
+  
 
-- Common workflows
+- Run the application without installing: `nix run github:aammfe/news-api` (or `nix run .` from checkout)
--- src/NewsAPI.hs
+++ src/NewsAPI.hs
@@ -1,59 +1,26 @@
 module NewsAPI where
 
-import App          (App(..), AppConfig(..))
+import App          (App(..), AppConfig(..), Cachekey)
+import APIData      (Response(..))
 
-import Data.Aeson   (decode, ToJSON, FromJSON)
+import Data.Aeson   (decode)
 import Network.HTTP.Client (newManager, parseRequest, httpLbs, responseBody, requestHeaders, Manager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Data.ByteString.Lazy  as LZB
-import Data.Text    (unpack)
+import Data.Text    (unpack, pack)
 import Web.HttpApiData  (FromHttpApiData(..), ToHttpApiData(..))
 import Network.URI.Encode   (encode)
-
-
-data Response = Response  
-    { status :: Text
-    , totalResults :: Int
-    , articles :: [Article]
-    } deriving stock (Eq, Show, Generic)
-
-
-instance ToJSON Response
-instance FromJSON Response
-
-
-data Source = Source
-    { id :: Maybe Text
-    , name :: Maybe  Text
-    } deriving stock (Eq, Show, Generic)
-
-instance ToJSON Source
-instance FromJSON Source
-
-
-data Article = Article
-    { source :: Maybe Source
-    , author :: Maybe Text
-    , title :: Maybe Text
-    , description :: Maybe Text
-    , url :: Maybe Text
-    , urlToImage :: Maybe Text
-    , publishedAt :: Maybe Text
-    , content :: Maybe Text
-    } deriving stock (Eq, Show, Generic)
-
-
-instance ToJSON Article
-instance FromJSON Article
+import Data.Cache   (insert, lookup, fetchWithCache) 
+import qualified Data.Cache   as C
 
 
 type PageNumber = Int
 type PageSize = Int
 
 
-fetchTopNewsArticles :: Maybe PageNumber -> Maybe PageSize -> App Response
-fetchTopNewsArticles pn ps = do
-    key <- asks newsAPIKey
+fetchTopNewsHeadlines :: Maybe PageNumber -> Maybe PageSize -> App Response
+fetchTopNewsHeadlines pn ps = do
+    key <- asks unNewsAPIKey
     fetch . topNewsUrl $ key
     where 
         topNewsUrl k
@@ -69,15 +36,17 @@ clientManager = liftIO . newManager $ tlsManagerSettings
 
 type URL = String
 
-fetch :: forall a .FromJSON a => URL -> App a
+fetch :: URL -> App Response
 fetch u = do
-    manager <- clientManager
-    request <- liftIO . parseRequest $ u
-    let userAgentHeader = ("User-Agent", "NewsAPI/1.0")
-        request1 = request { requestHeaders = userAgentHeader : requestHeaders request }
-    response <- liftIO $ httpLbs request1 manager
-    let jsonBody = responseBody response :: LZB.ByteString
-    return . fromMaybe (error ("error with api" <> show jsonBody)) . decode $ jsonBody
+    cache <- asks unCache
+    fetchWithCache cache (pack u) (\_ -> do
+        manager <- clientManager
+        request <- liftIO . parseRequest $ u
+        let userAgentHeader = ("User-Agent", "NewsAPI/1.0")
+            request1 = request { requestHeaders = userAgentHeader : requestHeaders request }
+        response <- liftIO $ httpLbs request1 manager
+        let jsonBody = responseBody response :: LZB.ByteString
+        return . fromMaybe (error ("error with api" <> show jsonBody)) . decode $ jsonBody)
 
 
 type Query = String
@@ -108,7 +77,7 @@ instance ToHttpApiData QueryBy where
 
 fetchSearchedArticles :: QueryBy -> Query -> Maybe PageNumber -> Maybe PageSize  -> App Response
 fetchSearchedArticles qb q pn ps = do
-    k <- asks newsAPIKey
+    k <- asks unNewsAPIKey
     fetch . searchUrl $ k
     where 
         searchUrl k
