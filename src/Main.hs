38eabc25228a9d9fe2b1db3305137fff78d19c95
Abdul Hanan <abdul.hanan@platonic.systems>
2023-07-19 00:47:01 +0000

    remove unwanted code

--- src/Main.hs
+++ src/Main.hs
@@ -10,8 +10,6 @@ import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = do
-  key <- readNewsApiKey
-  putStrLn key
   putStrLn "app is running on port:8080"
   let config = AppConfig { newsAPIKey = pack key}
   run 8080 (app config)
